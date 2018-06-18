c0ba48cac21a2a2db8795f183d25975b6f774e28
momo11 <mingmizzou@yahoo.com>
2018-06-17 20:54:02 -0500

    asdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import {Router, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 
+const routes = [
+
+ ]
 @NgModule({
   declarations: [
     AppComponent
@@ -11,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
